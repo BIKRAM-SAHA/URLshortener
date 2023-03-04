8da6d805760bde7b2738b18ecdbabc2b3942da19
BIKRAM-SAHA <bikramlearn15@gmail.com>
2023-03-04 13:55:25 +0530

    changed dbName

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 const conn2DB = async () => {
-  const connection = await mongoose.connect(process.env.MONGO_URI);
+  const connection = await mongoose.connect(process.env.MONGO_URI, {dbName: "urlshortener"});
   console.log(connection.connection.host);
 };
 
